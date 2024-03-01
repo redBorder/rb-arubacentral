b820aa2419e2fb33aaef747fdfec91dab306ef14
Miguel Negron <manegron@redborder.com>
2024-03-01 10:44:55 +0000

    Add modify version of aruba math

--- bin/helpers/aruba_math.rb
+++ bin/helpers/aruba_math.rb
@@ -16,11 +16,13 @@
 # Aruba central Math helpers to do some operations
 module ArubaMathHelper
   def self.move_coordinates_meters(east_movement, north_movement, lat, long)
+    corrector_north=2.136
+    corrector_east=2.068
     earth_major_radius = 6_378_137.0
     earth_minor_radius = 6_356_752.3
     radians_to_degrees = 180 / Math::PI
-    change_lat = north_movement / earth_minor_radius * radians_to_degrees
-    change_long = east_movement / (earth_major_radius * Math.cos(lat / radians_to_degrees)) * radians_to_degrees
+    change_lat = (north_movement*corrector_north) / earth_minor_radius * radians_to_degrees
+    change_long = (east_movement*corrector_east) / (earth_major_radius * Math.cos(lat / radians_to_degrees)) * radians_to_degrees
     new_lat = lat + change_lat
     new_long = long + change_long
     [new_lat, new_long]
